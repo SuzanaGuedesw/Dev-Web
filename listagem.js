aadc0bbdcbde8c1cbaea3c6c6cceba7a092fb031
Suzana Guedes <vieirasuzana336@gmail.com>
2025-06-02 19:05:54 -0300

    listagem

---
+++ listagem.js
@@ -0,0 +1,17 @@
+async function listagem(){
+
+    let token = JSON.parse(localStorage.getItem("user"));
+    console.log(token.access_token)
+
+    let api = await fetch("https://go-wash-api.onrender.com/api/auth/address",{
+        method:"GET",
+        headers: {
+                "Content-Type": "application/json", "Authorization": "Bearer "+token.access_token
+            }
+    });
+    if(api.ok){
+        let = response = await api.json()
+        console.log(response)
+    }
+}
+listagem()
